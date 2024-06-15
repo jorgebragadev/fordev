848d6f114c3d68d6ef2ff7b9b26452ddc1d15c66
Jorge Braga <jorgeabbraga@gmail.com>
2024-06-15 16:11:31 -0300

    move SUT creation to a setup method

--- test/data/usecases/remote_authentication_test.dart
+++ test/data/usecases/remote_authentication_test.dart
@@ -1,14 +1,18 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
-import 'dart:io';
-import 'package:meta/meta.dart';
-
+// remote_authentication_test.dart
 import 'package:faker/faker.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mockito/mockito.dart';
+import 'package:mocktail/mocktail.dart';
+
+// Importar as classes necessárias
+
+// Classes e Interfaces
+abstract class HttpClient {
+  Future<void> request({required String url, required String method});
+}
 
 class RemoteAuthentication {
   final HttpClient httpClient;
-  String url;
+  final String url;
 
   RemoteAuthentication({
     required this.httpClient,
@@ -20,18 +24,27 @@ class RemoteAuthentication {
   }
 }
 
-abstract class HttpClient {
-  Future<void> request({required String url, required String method});
-}
-
-class HttpClientSpy extends Mock implements HttpClient {}
+// Implementação do Mock usando Mocktail
+class MockHttpClient extends Mock implements HttpClient {}
 
 void main() {
-  test('Shoud call HttpClient with corret values', () async {
-    final httpClient = HttpClientSpy();
-    final url = faker.internet.httpUrl();
-    final sut = RemoteAuthentication(httpClient: httpClient, url: url);
+  late RemoteAuthentication sut;
+  late MockHttpClient httpClient;
+  late String url;
+
+  setUp(() {
+    httpClient = MockHttpClient();
+    url = faker.internet.httpUrl();
+    sut = RemoteAuthentication(httpClient: httpClient, url: url);
+
+    // Configurar o mock para retornar um Future<void>
+    when(() => httpClient.request(
+        url: any(named: 'url'),
+        method: any(named: 'method'))).thenAnswer((_) async => Future.value());
+  });
+
+  test('Should call HttpClient with correct values', () async {
     await sut.auth();
-    verify(httpClient.request(url: url, method: 'post'));
+    verify(() => httpClient.request(url: url, method: 'post')).called(1);
   });
 }
