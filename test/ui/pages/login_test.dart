729951b435e616865b405db667f9ddf67c2a258d
Jorge Braga <jorgeabbraga@gmail.com>
2024-06-23 17:26:34 -0300

    ensure LoginPage disables button is form is invalid

--- test/ui/pages/login_test.dart
+++ test/ui/pages/login_test.dart
@@ -166,4 +166,16 @@ void main() {
     expect(button.onPressed, isNotNull);
 
   });
+
+   testWidgets('Should enable button if form is valid',
+      (WidgetTester tester) async {
+    await loadPage(tester);
+
+    isFormValidController.add(false);
+    await tester.pump();
+
+        final button = tester.widget<ElevatedButton>(find.byType(ElevatedButton));
+    expect(button.onPressed, null);
+
+  });
 }
